7e4dbc5813c6de7bb92c7b6cff81240de97c27ca
max gunko <smouge1989@mail.ru>
2021-11-01 19:16:54 +0500

    added saving in localstorage

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,6 +1,9 @@
 import { ICard } from '../Card/Card';
 import styled from "styled-components";
 import { Column } from '../Column';
+import { Popup } from '../Popup';
+import { WelcomeModal } from '../WelcomeModal/WelcomeModal';
+import { useEffect, useState } from 'react';
 
 export interface IBoardProps {
 }
@@ -10,13 +13,28 @@ export interface ITodoList {
   cards: Array<ICard>
 }
 
-const lists: Array<ITodoList> =
+const defaultColumns: Array<ITodoList> =
   [{ title: "Todo", cards: [{ title: "Сходить в магазин" }, { title: "Полить цветы" }] },
   { title: "Todo", cards: [{ title: "Сходить в магазин" }, { title: "Полить цветы" }] },
   { title: "Todo", cards: [{ title: "Сходить в магазин" }, { title: "Полить цветы" }] },
   { title: "Todo", cards: [{ title: "Сходить в магазин" }, { title: "Полить цветы" }] }];
 
+const name: string | null = localStorage.getItem('name'); 
+
 export default function Board(props: IBoardProps) {
+
+  const [visible, setVisible] = useState(!!name)
+  const [lists, setList] = useState<Array<ITodoList>>();
+
+  useEffect(() => {
+    const storageLists = localStorage.getItem('lists')
+    if(storageLists === null) {
+      localStorage.setItem('lists', JSON.stringify(defaultColumns));
+      setList(defaultColumns)
+    } else {
+      setList(JSON.parse(storageLists));
+    }
+  }, []);
 
   const setTitle = (text: string) => {
     //здесь потом реализуем запись в локал сторадж
@@ -24,11 +42,18 @@ export default function Board(props: IBoardProps) {
     console.log(text)
   }
 
-  const columns = lists.map((list, index) => <Column key={index} setTitle={setTitle} title={list.title} cards={list.cards}/>)
+  const columns = lists && lists.map((list, index) => <Column key={index} setTitle={setTitle} title={list.title} cards={list.cards}/>)
+
+  const saveName = (name: string) => {
+    localStorage.setItem('name', name);
+    setVisible(true);
+  }
 
   return (
     <BoardWrapper>
+      <WelcomeModal saveName={saveName} visible = {visible}/>
       {columns}
+      {/* {!name ? <Popup visible/> : {columns}} */}
     </BoardWrapper>
   );
 }
